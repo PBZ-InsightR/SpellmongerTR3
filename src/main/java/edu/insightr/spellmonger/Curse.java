498822e4f1000c01722458d8aa71a76b8a2f3d39
vincentledoux <vincen.ledoux@gmail.com>
2016-09-21 12:35:55 +0200

    Ajout de la partie Rituel

---
+++ src/main/java/edu/insightr/spellmonger/Curse.java
@@ -0,0 +1,26 @@
+package edu.insightr.spellmonger;
+
+/**
+ * Created by Vincent on 21/09/2016. Define Curse
+ */
+public class Curse extends Ritual {
+
+
+    /**
+     * @param power The amout of Curse points
+     */
+    public Curse(int power) {
+        super(power);
+    }
+
+    /**
+     * @return the power of damage dealt
+     */
+    @Override
+    public String toString()
+    {
+        return "You loose " + getPower() + " life points. ";
+    }
+
+
+}
