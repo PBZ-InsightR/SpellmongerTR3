ccf7f6269f3c5d04b461fe2b7507359077a148bf
Yasmeen1 <yasmeen.trifiss@gmail.com>
2016-09-21 12:46:21 +0200

    class Ritual

---
+++ src/main/java/edu/insightr/spellmonger/Ritual.java
@@ -0,0 +1,22 @@
+package edu.insightr.spellmonger;
+
+/**
+ * Created by Yasmeen on 21/09/2016.
+ * isCurse (curse or blessing)
+ */
+public class Ritual {
+    private boolean isCurse;
+    private int lifePoints;
+
+    public Ritual(boolean isCurse, int lifePoints){
+        this.isCurse=isCurse;
+        this.lifePoints=lifePoints;
+    }
+
+    public int getLifePoints(){return this.lifePoints;}
+
+    public void updatePoints(){
+        if(isCurse && lifePoints!=0){ lifePoints-=3;}
+        if(!isCurse){ lifePoints+=3; }
+    }
+}
