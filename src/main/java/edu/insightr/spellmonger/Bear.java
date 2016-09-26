15eaca5e4741553827267f768a90dadd469a517e
Tara <zhongtara@gmail.com>
2016-09-26 20:17:58 +0200

    Change the constructor of SpellmongerApp (adding player's name + Display the cardPool List (can be removed))
    Separation of the initial function DrawACard in 2 functions :
    - drawACard (which only draws a card in the card Pool)
    - PlayACard (which plays the card entered in parameters)
    
    + Documentation

--- src/main/java/edu/insightr/spellmonger/Bear.java
+++ src/main/java/edu/insightr/spellmonger/Bear.java
@@ -4,7 +4,7 @@ package edu.insightr.spellmonger;
  * Created by thomas.Rousse on 21/09/2016.
  * define Bear
  */
-class Bear extends Beast {
+public class Bear extends Beast {
 
     public Bear(){
         super(3);
