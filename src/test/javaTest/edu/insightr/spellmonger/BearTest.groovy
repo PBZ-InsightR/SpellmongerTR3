95b354c063ee9f27c4a2e2cb499b81828c4d7b42
Stanislas Daniel Claude Dolcini <stanislas.dolcini@devinci.fr>
2016-09-27 19:48:45 +0200

    Start writing tests for the bear, doesn't compile yet.

---
+++ src/test/javaTest/edu/insightr/spellmonger/BearTest.groovy
@@ -0,0 +1,11 @@
+package edu.insightr.spellmonger
+
+/**
+ * Basic test for Bear class
+ * Created by Stan on 27/09/2016.
+ */
+class BearTest extends GroovyTestCase {
+    void testToString() {
+
+    }
+}
