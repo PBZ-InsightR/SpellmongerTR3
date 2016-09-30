06d791abe45a66a132389482047537b332f656b7
Hugues <hugues.rieublandou@devinci.fr>
2016-09-30 18:42:39 +0200

    Modification made by Hugues 30/09/2016
    DONE :
       - Added a little bit of documentation

--- src/main/java/edu/insightr/spellmonger/Beast.java
+++ src/main/java/edu/insightr/spellmonger/Beast.java
@@ -17,6 +17,10 @@ class Beast extends PlayCard {
         super(name, damage);
     }
 
+    /**
+     * Creates a string to describe the card. This is an override of the toString() function in the Card class.
+     * @return the description of the card (String)
+     */
     @Override
     public String toString() {
         return getName() +" : deals " + getDamage() + " damage";
