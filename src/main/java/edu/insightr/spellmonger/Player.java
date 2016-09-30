73e072584648c90e205f6c65c2ab03f41f99ad13
Hugues <hugues.rieublandou@devinci.fr>
2016-09-30 17:20:49 +0200

    Modification made by Hugues 30/09/2016
    DONE :
       - Added functions to manage :
           - Beast collection
           - Graveyard
           - Damage dealing

--- src/main/java/edu/insightr/spellmonger/Player.java
+++ src/main/java/edu/insightr/spellmonger/Player.java
@@ -1,31 +1,62 @@
 package edu.insightr.spellmonger;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by Yasmeen on 28/09/2016.
  *
  * Defines a player
  */
-public class Player {
+class Player {
 
     private String name;
     private int lifePoints;
-    private List<PlayCard> playerCreatures;
+    private ArrayList<PlayCard> playerCreatures;
+    private ArrayList<PlayCard> graveyard;
 
-    public Player(String name, int lifePoints){
+    Player(String name, int lifePoints){
         this.name=name;
         this.lifePoints=lifePoints;
-        playerCreatures=new ArrayList<>(35);
+        playerCreatures=new ArrayList<>();
+        graveyard = new ArrayList<>();
     }
 
-    public String getName(){return name;}
-    public int getLifePoints(){return lifePoints;}
-    public void setLifePoints(int value){lifePoints=value;}
 
-    public void addCreature(PlayCard card){
+    String getName(){return name;}
+    int getLifePoints(){return lifePoints;}
+
+    /**
+     * Adds a creature in the Beast list of the player
+     * @param card : the Beast to put
+     */
+    void addCreature(PlayCard card){
         this.playerCreatures.add(card);
+    }
+
+    /**
+     * Returns all the creatures
+     * @return a list containing the creatures of the player
+     */
+    ArrayList<PlayCard> getCreatures(){
+        // To make sure one can not modify our list freely, we return a clone
+        ArrayList<PlayCard> clone = new ArrayList<>(this.playerCreatures.size());
+        clone.addAll(this.playerCreatures);
+        return clone;
+    }
+
+    void inflictDamages(int damage){this.lifePoints-=damage;}
+
+    /**
+     *
+     * @param card : the card to put in the graveyard
+     * @return true if the card was put in the graveyard, false otherwise
+     */
+    boolean addToGraveyard(PlayCard card){
+        if (card != null){
+            graveyard.add(card);
+            return true;
+        }
+        return false;
     }
 
 }
