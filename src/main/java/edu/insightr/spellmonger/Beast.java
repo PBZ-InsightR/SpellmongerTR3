8911b119e78cba936f51e08b4368a2678bcfdc94
Hugues <hugues.rieublandou@devinci.fr>
2016-09-30 17:24:17 +0200

    Modification made by Hugues 30/09/2016
    DONE :
       - Got rif of Type
       - Simplified greatly the constructor
       - Made a slightly more appealing ToString()

--- src/main/java/edu/insightr/spellmonger/Beast.java
+++ src/main/java/edu/insightr/spellmonger/Beast.java
@@ -4,44 +4,22 @@ package edu.insightr.spellmonger;
  * Created by Stanislas Daniel Claude D on 21/09/2016.
  * Defines a standard beast
  */
-public abstract class Beast extends PlayCard {
-
-    private String type;
-
-
+class Beast extends PlayCard {
 
 
     /**
-     * @param type defined the beast as  a bear an eagle or a wolf
+     * @param name is the name of the beast
      * @param damage is an heritage from the constructor of PlayCard
 
      *
      */
-    public Beast(int damage, String type) {
-        super("creature",damage);
-        this.type = type;
-       /**
-        * if we need in the next steps a switch , find it bellow:
-        * switch(type_switch){
-            case wolf: type="wolf";
-                break;
-            case bear: type="bear";
-                break;
-            case eagle :type="eagle";
-                brak;
-        }**/
-
+    Beast(String name, int damage) {
+        super(name, damage);
     }
 
-    public String getType() {
-        return this.type;
-    }
     @Override
     public String toString() {
-        
-        return "This Card named"+ this.name+"deals" + this.damage + " damage ."+"This is a beast"+this.type;
+        return getName() +" : deals " + getDamage() + " damage";
     }
-    /**
-     * @return description string of the class.
-     */
+
 }
