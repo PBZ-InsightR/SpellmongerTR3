a70812ddbf911881f56fd9660d157b89ca44f1cc
Stanislas Daniel Claude Dolcini <stanislas.dolcini@devinci.fr>
2016-10-02 15:59:59 +0200

    Add Test Architecture + Example Test Class

---
+++ src/test/java/edu/insightr/spellmonger/PlayerTest.java
@@ -0,0 +1,37 @@
+package edu.insightr.spellmonger;
+
+import org.junit.Before;
+import org.junit.Assert;
+import static org.junit.Assert.*;
+
+/**
+ * Standard Test class for the player class.
+ * Created by Stan on 02/10/2016.
+ */
+public class PlayerTest {
+    private Player playerA;
+    @Before
+    public void initialize() {
+        this.playerA = new Player("Alice",20);
+    }
+
+    @org.junit.Test
+    public void getName() throws Exception {
+        Assert.assertEquals(this.playerA.getName(),"Alice");
+    }
+
+    @org.junit.Test
+    public void getLifePoints() throws Exception {
+        Assert.assertEquals(this.playerA.getLifePoints(),20);
+    }
+
+    @org.junit.Test
+    public void inflictDamages() throws Exception {
+        Assert.assertEquals(this.playerA.getLifePoints(),20);
+        this.playerA.inflictDamages(5);
+        Assert.assertEquals(this.playerA.getLifePoints(),15);
+        this.playerA.inflictDamages(-2);
+        Assert.assertEquals(this.playerA.getLifePoints(),17);
+    }
+
+}
