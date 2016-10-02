2612db90306af81f0e3bf90b4b34d11b9653b4c4
Stanislas Daniel Claude Dolcini <stanislas.dolcini@devinci.fr>
2016-10-02 22:49:44 +0200

    Beast tests

---
+++ src/test/java/edu/insightr/spellmonger/BeastTest.java
@@ -0,0 +1,30 @@
+package edu.insightr.spellmonger;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Standard Beast test
+ * Created by Stan on 02/10/2016.
+ */
+public class BeastTest {
+
+    private Beast beast;
+    @Before
+    public void initialize() {
+        this.beast = new Beast("Eagle",1);
+    }
+
+    @Test
+    public void getDamage() throws Exception {
+        assertEquals(beast.getDamage(),1);
+    }
+
+    @Test
+    public void getName() throws Exception {
+        assertEquals(beast.getName(),"Eagle");
+    }
+
+}
