47e7e8b5f69345207e89c1c38294971893393fce
Hugues <hugues.rieublandou@devinci.fr>
2016-10-07 11:09:03 +0200

    Changed the Activae() function to take the damage blocked into account

--- src/main/java/edu/insightr/spellmonger/Beast.java
+++ src/main/java/edu/insightr/spellmonger/Beast.java
@@ -20,10 +20,22 @@ class Beast extends PlayCard {
 
     @Override
     public void activate(SpellmongerApp app){
-        app.getCurrentPlayer().addCreature(this);
-        logger.info(app.getCurrentPlayer().getName() + " plays a Beast. It is a " + this.getName());
-
+        //app.getCurrentPlayer().addCreature(this);
+        this.activate(app, 0);
     }
+
+    /**
+     * Activates the beast (used if some damages are blocked)
+     * @param app : the game engine (SpellmongerApp)
+     * @param damageBlocked : the amount of damages blocked (int)
+     */
+    void activate(SpellmongerApp app, int damageBlocked){
+        //app.getCurrentPlayer().addCreature(this);
+        int damageDealt = this.getDamage() - damageBlocked;
+        app.getOpponent().inflictDamages(damageDealt);
+        logger.info(" " + app.getCurrentPlayer().getName() + "'s " + this.getName() + " deals " + damageDealt + " damage(s) to " + app.getOpponent().getName() + ".");
+    }
+
     /**
      * Creates a string to describe the card. This is an override of the toString() function in the Card class.
      *
