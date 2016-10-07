99ea6cdd0fc9d3811bfa82defa41bafaebb26f85
Hugues <hugues.rieublandou@devinci.fr>
2016-10-07 09:50:01 +0200

    Small grammatical correction

--- src/main/java/edu/insightr/spellmonger/CardActivation.java
+++ src/main/java/edu/insightr/spellmonger/CardActivation.java
@@ -2,7 +2,7 @@ package edu.insightr.spellmonger;
 
 /**
  * Created by thomas.Rousse on 05/10/2016.
- * implement the action of the PlayCard's Childrens .
+ * implement the action of the PlayCard's Children .
  */
 interface CardActivation {
 
