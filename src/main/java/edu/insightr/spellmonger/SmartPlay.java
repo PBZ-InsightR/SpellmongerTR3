7d55c327f6c9783c1fe8058b6b7d8b30f2b2d5f8
sibelsirac <sibel.sirac@devinci.fr>
2016-10-10 12:06:47 +0200

    creation of the class smartplay

---
+++ src/main/java/edu/insightr/spellmonger/SmartPlay.java
@@ -0,0 +1,16 @@
+package edu.insightr.spellmonger;
+
+/**
+ * Created by sibel on 10/10/2016.
+ */
+public class SmartPlay {
+    /**
+     *
+     * @param playerA
+     * @param spell game
+     */
+    public SmartPlay(Player playerA, SpellmongerApp spell){
+
+//Player.Playcard(spell);
+    }
+}
