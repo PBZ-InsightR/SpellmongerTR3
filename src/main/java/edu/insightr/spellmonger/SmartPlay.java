703edd6722347abe8faa526c64a6bb0b670e4a7a
sibelsirac <sibel.sirac@devinci.fr>
2016-10-10 12:06:47 +0200

    creation of the class smartplay

--- src/main/java/edu/insightr/spellmonger/SmartPlay.java
+++ src/main/java/edu/insightr/spellmonger/SmartPlay.java
@@ -1,59 +1,16 @@
 package edu.insightr.spellmonger;
 
-import java.util.Random;
-
 /**
  * Created by sibel on 10/10/2016.
- * Define the Levels of the IA for the application
  */
-public class SmartPlay extends Player{
-
-    private int level;
+public class SmartPlay {
     /**
      *
-     * @param level of the game
+     * @param playerA
+     * @param spell game
      */
-    public SmartPlay(String name, int lifePoints, int level){
-        super(name,lifePoints);
-        this.level = level;
-    }
+    public SmartPlay(Player playerA, SpellmongerApp spell){
 
-    /**
-     * @return a random number that will be the position of the card in the hand of a player
-     */
-    int level0(){
-        int size=getHand().size();
-        Random rnd=new Random();
-        int playcardNumber=rnd.nextInt(size);
-        return playcardNumber;
-    }
-
-    /**
-     * Ovveride the play of a card from playcard, with the IA
-     *
-     * @param game the app
-     */
-    @Override
-    void playACard(SpellmongerApp game) {
-        int playCardNumber;
-
-        switch(level)
-        {
-            case 0:
-
-
-            default:
-                playCardNumber=level0();
-                break;
-
-        }
-
-        PlayCard card =getHand().get(playCardNumber);
-        card.setOwner(this);
-        getHand().remove(playCardNumber);
-        game.playCard(card);
-       /* PlayCard card = this.cardsInHand.get(this.cardsInHand.size() - 1);
-        card.setOwner(this);
-        this.cardsInHand.remove(this.cardsInHand.size() - 1);*/
+//Player.Playcard(spell);
     }
 }
