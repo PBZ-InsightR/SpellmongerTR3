6f1822e260ab748c68acac01a29df7d34023636d
scoutantho <scoutantho78200@gmail.com>
2016-10-13 18:34:26 +0200

    hello world javaFX, importing by an other new project

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,34 +1,20 @@
 package sample;
 
-import edu.insightr.spellmonger.SpellmongerApp;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.apache.log4j.Logger;
-
-import java.lang.reflect.Executable;
-import java.net.URL;
+import java.lang.*;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Logger logger = Logger.getLogger(SpellmongerApp.class);
-
-        URL url = this.getClass().getResource("/sample.fxml");
-        logger.info(url);
-        try{
-        Parent root = FXMLLoader.load(url);
+    public void start(Stage primaryStage) {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
-        }
-        catch(Exception ex)
-        {
-            logger.error("File not found");
-        }
     }
 
 
