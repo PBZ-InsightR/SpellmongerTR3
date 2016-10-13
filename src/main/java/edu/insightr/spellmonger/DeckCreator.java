40af8c3dd54f251ccf1a951a56b9056cc045da62
Yasmeen1 <yasmeen.trifiss@gmail.com>
2016-10-13 22:28:11 +0200

    DeckCreator adapt to the new rules

--- src/main/java/edu/insightr/spellmonger/DeckCreator.java
+++ src/main/java/edu/insightr/spellmonger/DeckCreator.java
@@ -12,7 +12,20 @@ import java.util.*;
  */
 class DeckCreator {
     private static final Logger logger = Logger.getLogger(SpellmongerApp.class);
-
+    private static final List<PlayCard> cardList= new ArrayList<>(Arrays.asList(
+            new Beast(SpellmongerApp.cardNameBear, 3),
+            new Beast(SpellmongerApp.cardNameWolf, 2),
+            new Beast(SpellmongerApp.cardNameEagle, 1),
+            new Ritual(SpellmongerApp.cardNamePoison, 3, false, true),
+            new Ritual(SpellmongerApp.cardNameHeal, -3, true, true),
+            new Ritual(SpellmongerApp.cardNameShield,0,true, false)
+        ));
+    private static final int numBear=10;
+    private static final int numWolf=10;
+    private static final int numEagle=10;
+    private static final int numPoison=2;
+    private static final int numHeal=3;
+    private static final int numShield=5;
 
     /**
      * The main function. Returns a full card deck
@@ -21,133 +34,41 @@ class DeckCreator {
      */
     static List<PlayCard> fillCardPool(int maxNumberOfCard) {
 
-        List<Integer> results = distribution(maxNumberOfCard);
-        int totalBeast = results.get(0);
-        int totalRitual = results.get(1);
-        int[] numberOfBeast = distributionBeast(totalBeast);   // random numbers of beasts for each type of beast
-        int numberOfRitual = Math.round(totalRitual / 2);       // number of rituals for each type of ritual
-        int counterBeastType = 0;
-
 
         List<PlayCard> cardPool = new ArrayList<>();
         // Filling the cardPool List
         // IMPORTANT : We add a clone and not the card itself (otherwise, they would share the same
         // address!
-        List<PlayCard> cardsList = generateCardList();
 
-        for (PlayCard card : cardsList) {
-            if (card.getClass().equals(Beast.class)) {
-                for (int i = 0; i < numberOfBeast[counterBeastType]; ++i) {
-                    cardPool.add((PlayCard) card.clone());
-                }
-                ++counterBeastType;
-            } else if (card.getClass().equals(Ritual.class)) {
-                for (int i = 0; i < numberOfRitual; ++i) {
-                    cardPool.add((PlayCard) card.clone());
-                }
-            }
+        for (int i = 0; i < numBear; ++i) {
+            cardPool.add((PlayCard) cardList.get(0).clone());
+        }
+        for (int i = 0; i < numWolf; ++i) {
+            cardPool.add((PlayCard) cardList.get(1).clone());
+        }
+        for (int i = 0; i < numEagle; ++i) {
+            cardPool.add((PlayCard) cardList.get(2).clone());
         }
 
-        shuffleCardPool(cardPool);
+        for (int i = 0; i < numPoison; ++i) {
+            cardPool.add((PlayCard) cardList.get(3).clone());
+        }
+        for (int i = 0; i < numHeal; ++i) {
+            cardPool.add((PlayCard) cardList.get(4).clone());
+        }
+        for (int i = 0; i < numShield; ++i) {
+            cardPool.add((PlayCard) cardList.get(5).clone());
+        }
+        Collections.shuffle(cardPool);
 
 
         // For Tests : Display the cardPool list
         logger.info("\n");
-        logger.info(SpellmongerApp.cardNameBear + " : " + numberOfBeast[0] + "    " + SpellmongerApp.cardNameWolf +": " + numberOfBeast[1] + "    "+ SpellmongerApp.cardNameEagle +" : " + numberOfBeast[2]);
-        logger.info(SpellmongerApp.cardNamePoison + "/" + SpellmongerApp.cardNameHeal+"/"+ SpellmongerApp.cardNameShield + ": " + numberOfRitual + " for each");
         logger.info("CardPool : " + cardPool);
-        logger.info("Size of CardPool : " + cardPool.size());
 
         return cardPool;
     }
 
-    /**
-     * Creates the list of possible card (change this to change the available cards in the game
-     *
-     * @return the list of cards (ArrayList)
-     */
-    private static List<PlayCard> generateCardList() {
-        final List<PlayCard> cardList;
-        cardList = new ArrayList<>(Arrays.asList(
-                new Beast(SpellmongerApp.cardNameBear, 3),
-                new Beast(SpellmongerApp.cardNameWolf, 2),
-                new Beast(SpellmongerApp.cardNameEagle, 1),
-                new Ritual(SpellmongerApp.cardNamePoison, 3, false, true),
-                new Ritual(SpellmongerApp.cardNameHeal, -3, true, true),
-                new Ritual(SpellmongerApp.cardNameShield,0,true, false)
-        ));
 
-        return cardList;
     }
 
-
-    /**
-     * Return a list of numbers
-     * The first number is the number of monsters [0]
-     * The second number is the number of rituals [1]
-     *
-     * @param numberOfCard : input of the number of card
-     * @return {@code List<int>} of the repartition of cards
-     */
-    private static List<Integer> distribution(int numberOfCard) {
-        List<Integer> results = new ArrayList<>();
-
-        int monsters;
-        int rituals;
-        int total;
-
-        monsters = (numberOfCard * 5) / 6;
-        rituals = numberOfCard / 6;
-        total = monsters + rituals;
-
-        // Make sure there is always an even number of
-        // curse and blessings
-        if (total != numberOfCard) {
-            if (rituals % 2 == 0) {
-                ++monsters;
-            } else {
-                ++rituals;
-            }
-        } else {
-            if (rituals % 2 != 0) {
-                ++rituals;
-                --monsters;
-            }
-        }
-
-        results.add(monsters);
-        results.add(rituals);
-        return results;
-    }
-
-    /**
-     * Generates 3 random numbers, whose sum is the total number of beasts
-     * The first number (x) is the number of Bears
-     * The second number (y) is the number of Wolfs
-     * The third number (z) is the number of Eagles
-     * There is 58 beast, 58/3 = 19 average of each beast
-     *
-     * @param numCard : input of the total number of beasts
-     * @return {@code int[]} of the repartition of beasts
-     */
-    private static int[] distributionBeast(int numCard) {
-        int min = Math.round(numCard / 4); //the minimum number of beast will be 15
-        int max = Math.round((int) (numCard / 2.5));  //the maximum will be 23
-        Random randomNumX = new Random();
-        Random randomNumY = new Random();
-        int numBears = randomNumX.nextInt(max - min + 1) + min;
-        int numWolves = randomNumY.nextInt(max - min + 1) + min;
-        int numEagles = numCard - numBears - numWolves;
-        return new int[]{numBears, numWolves, numEagles};
-    }
-
-
-    /**
-     * Shuffles the cards pool given as a parameter
-     *
-     * @param cardPool : the list of cards (List)
-     */
-    private static void shuffleCardPool(List cardPool) {
-        Collections.shuffle(cardPool);
-    }
-}
