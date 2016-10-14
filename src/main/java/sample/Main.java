4be8e694123353744addf68832120ca2e5b27c4b
guillaume lesieur <guillaumelesieur4@gmail.com>
2016-10-14 10:20:11 +0200

    small changes in javafx main

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -3,8 +3,10 @@ package sample;
 import edu.insightr.spellmonger.SpellmongerApp;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import org.apache.log4j.Logger;
 
@@ -20,10 +22,16 @@ public class Main extends Application {
         URL url = this.getClass().getResource("/sample.fxml");
         logger.info(url);
         try{
-        Parent root = FXMLLoader.load(url);
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+            Parent root = FXMLLoader.load(url);
+            primaryStage.setTitle("SpellMonger");
+            primaryStage.setScene(new Scene(root, 300, 275));
+
+            // la racine est le root
+
+
+
+
+            primaryStage.show();
         }
         catch(Exception ex)
         {
