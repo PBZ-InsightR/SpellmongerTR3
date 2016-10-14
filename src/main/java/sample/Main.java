af6e18c8e022718830ecf349acfd659675ec14f4
Stanislas Daniel Claude Dolcini <stanislas.dolcini@devinci.fr>
2016-10-14 09:09:16 +0200

    Fix HelloWorld graphical interface

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -1,20 +1,34 @@
 package sample;
 
+import edu.insightr.spellmonger.SpellmongerApp;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import java.lang.*;
+import org.apache.log4j.Logger;
+
+import java.lang.reflect.Executable;
+import java.net.URL;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) {
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    public void start(Stage primaryStage) throws Exception{
+        Logger logger = Logger.getLogger(SpellmongerApp.class);
+
+        URL url = this.getClass().getResource("/sample.fxml");
+        logger.info(url);
+        try{
+        Parent root = FXMLLoader.load(url);
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+        }
+        catch(Exception ex)
+        {
+            logger.error("File not found");
+        }
     }
 
 
