fe947d66e779924ba8c20097ed569424ab51a53d
scoutantho <scoutantho78200@gmail.com>
2016-10-19 23:28:04 +0200

    Add picts of card for prof's test

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -44,10 +44,13 @@ public class Main extends Application {
         Image img = new Image(getClass().getResourceAsStream("/img.jpg"));
         Image img2 = new Image(getClass().getResourceAsStream("/img2.jpg"));
         Image img3 = new Image(getClass().getResourceAsStream("/img3.jpg"));
-
+        Image img3_2 = new Image(getClass().getResourceAsStream("/img3_2.jpg"));
+        Image img3_3 = new Image(getClass().getResourceAsStream("/img3_3.jpg"));
+        Image img3_4 = new Image(getClass().getResourceAsStream("/img3_4.jpg"));
+        Image img3_5 = new Image(getClass().getResourceAsStream("/img3_5.jpg"));
         //Set the go for open Window V_BoardCard
         Button go = new Button("GO");
-        go.setOnAction(e-> V_BoardCard.display(img,img2,img3));
+        go.setOnAction(e-> V_BoardCard.display(img,img2,img3,img3_2,img3_3,img3_4,img3_5));
 
         //add button and set scene
         StackPane layout = new StackPane();
