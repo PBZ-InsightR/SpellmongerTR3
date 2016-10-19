6b39a7559f08d67afc1fbf80a7cc9ea79e8673c8
scoutantho <scoutantho78200@gmail.com>
2016-10-19 23:17:38 +0200

    Add AlertBox, Add C_board not use, Change details

---
+++ src/main/java/sample/C_BoardCard.java
@@ -0,0 +1,10 @@
+package sample;
+
+/**
+ * Created by antho on 19/10/2016.
+ * do for get children of V_boardCard, and for example do the same thing for all button "card"
+ */
+public class C_BoardCard {
+
+
+}
--- src/main/java/sample/V_BoardCard.java
+++ src/main/java/sample/V_BoardCard.java
@@ -1,5 +1,6 @@
 package sample;
 
+import com.sun.javafx.util.TempState;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -9,6 +10,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 /**
@@ -18,9 +20,12 @@ import javafx.stage.Stage;
 public class V_BoardCard {
 
     public static void display(Image img, Image img2, Image img3){
+
         Stage board = new Stage();
-        board.setMinHeight(1000);
-        board.setMinWidth(500);
+        board.getIcons().add(new Image("/lofo_esilv.png"));
+        board.initModality(Modality.APPLICATION_MODAL);
+        board.setMinHeight(500);
+        board.setMinWidth(1000);
         board.setTitle("SpellMonger");
 
 
@@ -32,7 +37,6 @@ public class V_BoardCard {
         Button btnLeft = new Button();
         btnLeft.setGraphic(new ImageView(img2));
 
-        Button button_img3 = new Button("",new ImageView(img3));
 
         //Set button at middle
         Button button_center = new Button();
@@ -41,10 +45,10 @@ public class V_BoardCard {
         button.setOnAction(e-> System.out.println("Draw a card"));
 */
 //set a grip pane not use for moment
-        GridPane grid = new GridPane();
+        /*GridPane grid = new GridPane();
         grid.setPadding(new Insets(10,10,10,10));
         grid.setVgap(8);
-        grid.setHgap(10);
+        grid.setHgap(10);*/
 
 
 //set Hbox for both top and bottom
@@ -57,27 +61,26 @@ public class V_BoardCard {
         topMenu.getChildren().addAll(button_card1,button_card2,button_card3,button_card4,button_card5);
 
         HBox botMenu = new HBox();
-        Button button_card6 = new Button("Card6");
-        Button button_card7 = new Button("Card7");
-        Button button_card8 = new Button("Card8");
-        Button button_card9 = new Button("Card9");
-        Button button_card10 = new Button("Card10");
-        topMenu.getChildren().addAll(button_card6,button_card7,button_card8,button_card9,button_card10);
+        Button button_card6 = new Button("", new ImageView(img3));
+        Button button_card7 = new Button("", new ImageView(img3));
+        Button button_card8 = new Button("", new ImageView(img3));
+        Button button_card9 = new Button("", new ImageView(img3));
+        Button button_card10 = new Button("", new ImageView(img3));
+        botMenu.getChildren().addAll(button_card6,button_card7,button_card8,button_card9,button_card10);
 
 
 
 //Set layout
+
         BorderPane layout =  new BorderPane();
 
         layout.setTop(topMenu);
         BorderPane.setAlignment(topMenu, Pos.TOP_CENTER);
 
         layout.setBottom(botMenu);
-        BorderPane.setAlignment(botMenu, Pos.TOP_CENTER);
+        BorderPane.setAlignment(botMenu, Pos.BOTTOM_CENTER);
 
         layout.setCenter(button_center);
-
-
 
         layout.setLeft(btnLeft);
         BorderPane.setAlignment(btnLeft, Pos.CENTER);
@@ -130,16 +133,66 @@ public class V_BoardCard {
             button_center.setGraphic(button_card5.getGraphic());
             button_card5.setGraphic(Temps.getGraphic());
         });
-
-        btnPlay.setOnAction(e->{
-            btnLeft.setGraphic(button_center.getGraphic());
-            button_center.setGraphic(null);
+        button_card6.setOnAction(e->{
+            Button Temps = new Button();
+            Temps.setText(button_center.getText());
+            Temps.setGraphic(button_center.getGraphic());
+            button_center.setGraphic(button_card6.getGraphic());
+            button_card6.setGraphic(Temps.getGraphic());
+        });
+        button_card7.setOnAction(e->{
+            Button Temps = new Button();
+            Temps.setText(button_center.getText());
+            Temps.setGraphic(button_center.getGraphic());
+            button_center.setGraphic(button_card7.getGraphic());
+            button_card7.setGraphic(Temps.getGraphic());
+        });
+        button_card8.setOnAction(e->{
+            Button Temps = new Button();
+            Temps.setText(button_center.getText());
+            Temps.setGraphic(button_center.getGraphic());
+            button_center.setGraphic(button_card8.getGraphic());
+            button_card8.setGraphic(Temps.getGraphic());
+        });
+        button_card9.setOnAction(e->{
+            Button Temps = new Button();
+            Temps.setText(button_center.getText());
+            Temps.setGraphic(button_center.getGraphic());
+            button_center.setGraphic(button_card9.getGraphic());
+            button_card9.setGraphic(Temps.getGraphic());
+        });
+        button_card10.setOnAction(e->{
+            Button Temps = new Button();
+            Temps.setText(button_center.getText());
+            Temps.setGraphic(button_center.getGraphic());
+            button_center.setGraphic(button_card10.getGraphic());
+            button_card10.setGraphic(Temps.getGraphic());
         });
 
+        btnPlay.setOnAction(e->{
+            if(button_center.getGraphic()!=null){
+            btnLeft.setGraphic(button_center.getGraphic());
+            button_center.setGraphic(null);}
+            else AlertBox.display("Invalide","Please select a card");
+        });
+
+
+
+
+        //Set scene and show it
         Scene scene = new Scene(layout);
         board.setScene(scene);
         board.showAndWait();
 
     }
 
+    //idée pour changer la récurrence de traitement des bouttons
+   /* public void PressButton(Button FromThere, Button WhereToGo){
+        Button temps = new Button();
+        temps.setGraphic(WhereToGo.getGraphic());
+        WhereToGo.setGraphic(FromThere.getGraphic());
+        FromThere.setGraphic(temps.getGraphic());
+        System.out.println(FromThere.getGraphic());
+        System.out.println(WhereToGo.getGraphic());
+    }*/
 }
