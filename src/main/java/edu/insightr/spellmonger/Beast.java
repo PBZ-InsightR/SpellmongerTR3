004496f1a97141a48f797492c83948fc5b460d9e
Stanislas Daniel Claude Dolcini <stanislas.dolcini@devinci.fr>
2016-10-31 13:44:20 +0100

    Remove dead code.

--- src/main/java/edu/insightr/spellmonger/Beast.java
+++ src/main/java/edu/insightr/spellmonger/Beast.java
@@ -17,26 +17,6 @@ class Beast extends PlayCard {
         super(name, damage, false, cardValue);
     }
 
-    /*
-    /**
-     * @param app The current game
-     */
-    /*
-    @Override
-    public void activate(SpellmongerApp app) {
-        Player target;
-        Player opponentPlayer = app.getOpponentPlayer();
-
-        if(this.getOwner().equals(opponentPlayer))
-            target = app.getCurrentPlayer();
-        else
-            target = opponentPlayer;
-
-        target.inflictDamages(this.getDamage());
-        logger.info(" " + this.getOwner().getName() + "'s " + this.getName() + " deals " + this.getDamage() + " damage(s) to " + target.getName() + ".");
-
-    }*/
-
     /**
      * Creates a string to describe the card. This is an override of the toString() function in the Card class.
      *
