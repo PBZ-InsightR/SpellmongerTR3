5a58692abcc3a65730013d905507bcf6f052824a
scoutantho <scoutantho78200@gmail.com>
2016-11-04 09:36:04 +0100

    change main to V_Menu

---
+++ src/main/java/sample/V_Menu.java
@@ -0,0 +1,55 @@
+package sample;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import static com.sun.org.apache.bcel.internal.util.SecuritySupport.getResourceAsStream;
+
+/**
+ * Created by antho on 04/11/2016.
+ */
+public class V_Menu {
+
+
+
+    public static void display(Stage primaryStage) {
+
+        //set Image
+        Image img = new Image(getResourceAsStream("/img.jpg"));
+        Image img2 = new Image(getResourceAsStream("/img2.jpg"));
+        Image img3 = new Image(getResourceAsStream("/img3.jpg"));
+        Image logo_go = new Image(getResourceAsStream("/go.png"));
+
+        primaryStage.setTitle("SpellmongerTR3");
+        primaryStage.getIcons().add(new Image("/logo_esilv.png"));
+
+        //set menubar
+        final javafx.scene.control.MenuBar menuBar = Usefull.MenuBar(primaryStage);
+
+        //Set the go for open Window V_BoardCard
+        Button go = new Button("Start");
+        go.setId("go");
+        go.setGraphic(new ImageView(logo_go));
+
+        go.setOnAction(e -> V_BoardCard.display(img, img2, img3, primaryStage));
+
+        //add button and set scene
+        BorderPane layout = new BorderPane();
+        layout.setId("layout");
+        layout.setTop(menuBar);
+        layout.setCenter(go);
+        BorderPane.setAlignment(go, Pos.CENTER);
+        Scene scene = new Scene(layout, 1000, 500);
+        scene.getStylesheets().add("style.css");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+
+    }
+}
