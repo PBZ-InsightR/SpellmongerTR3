a54ec82818cf06aa0e551cbdbc199138cddbe997
Tara <zhongtara@gmail.com>
2016-11-07 12:03:45 +0100

    Change architecture of Main

--- src/main/java/edu/insightr/spellmonger/Controller/Main.java
+++ src/main/java/edu/insightr/spellmonger/Controller/Main.java
@@ -1,9 +1,13 @@
 package edu.insightr.spellmonger.Controller;
 
+import edu.insightr.spellmonger.Model.SpellmongerApp;
 import edu.insightr.spellmonger.View.V_Menu;
 import javafx.application.Application;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main extends Application {
 
@@ -20,14 +24,29 @@ private static Stage primaryStage;
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+        /** Initialisation variable **/
         //set Image
         Image img = new Image(getClass().getResourceAsStream("/img.jpg"));
         Image img2 = new Image(getClass().getResourceAsStream("/img2.jpg"));
         Image img3 = new Image(getClass().getResourceAsStream("/img3.jpg"));
         Image logo_go = new Image(getClass().getResourceAsStream("/go.png"));
 
+        String playerA = "Alice";
+        String playerB = "Bob";
+
+        final int lifePoints = 20;
+        final int maxNumberOfCards = 40;
+        List<String> playersList = new ArrayList<>();
+        playersList.add(playerA);
+        playersList.add(playerB);
+        /** End Initialisation variable **/
+
+
+        SpellmongerApp model = new SpellmongerApp(playersList, lifePoints, maxNumberOfCards);
+        C_SpellmongerApp controller = new C_SpellmongerApp(model);
+        controller.runSpellmongerApp();
 
         V_Menu menu = new V_Menu();
-        V_Menu.display(img, img2, img3, logo_go, primaryStage);
+        V_Menu.display(img, img2, img3, logo_go, primaryStage);  // HAVE TO BE :  controller.display();
         }
 }
