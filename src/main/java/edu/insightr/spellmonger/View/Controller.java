0a5913f17d5775a616a13080a0a393e1b32548c7
Yasmeen1 <yasmeen.trifiss@gmail.com>
2016-11-11 13:33:19 +0100

    Add UI using JavaFX FXML

---
+++ src/main/java/edu/insightr/spellmonger/View/Controller.java
@@ -0,0 +1,45 @@
+package edu.insightr.spellmonger.View;
+
+/**
+ * Created by Yasmeen on 09/11/2016.
+ */
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    public TextField username1;
+    public TextField username2;
+    public Button goButton;
+    public Button clearButton;
+    public ToggleButton aiButton;
+    public Label actiontarget;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        System.out.println("View is loaded!");
+    }
+
+    public void go(ActionEvent actionEvent) {
+        if("".equals(username1.getText()) || "".equals(username2.getText())) actiontarget.setText("Field(s) empty");
+        else if(username1.getText().equals(username2.getText())) actiontarget.setText("Choose a different player");
+        else actiontarget.setText(username1.getText()+" VS "+username2.getText());
+    }
+
+    public void clearGame(ActionEvent actionEvent) {
+        username1.clear();
+        username2.clear();
+    }
+
+    public void addAI(ActionEvent actionEvent) {
+        if(aiButton.isSelected())username2.setText("PC");
+        else username2.clear();
+    }
+}
---
+++ src/main/java/edu/insightr/spellmonger/View/Main.java
@@ -0,0 +1,24 @@
+package edu.insightr.spellmonger.View;/**
+ * Created by Yasmeen on 09/11/2016.
+ */
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/spellmongerApp.fxml"));
+        primaryStage.setTitle("Spellmonger");
+        primaryStage.setScene(new Scene(root, 640, 480));
+        primaryStage.show();
+    }
+
+}
