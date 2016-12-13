6d5d93fa45e6f1abe3bd8b664e1e9b212e4258fc
vincentledoux <vincen.ledoux@gmail.com>
2016-12-13 14:16:02 +0100

    Beginning of Test on IAT_level2

---
+++ src/test/java/edu/insightr/spellmonger/Model/SmartPlayer_level2Test.java
@@ -0,0 +1,16 @@
+package edu.insightr.spellmonger.Model;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Vincent on 13/12/2016.
+ */
+public class SmartPlayer_level2Test {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+}
